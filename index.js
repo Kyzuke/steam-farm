f32d16baf6129b0dec5185582d39535e6a240849
Gabriel Silva <107601526+Kyzuke@users.noreply.github.com>
2025-01-26 04:03:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var password = process.env.password;
 var shared_secret = process.env.shared;
 
 var games = [2634950, 254700];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
